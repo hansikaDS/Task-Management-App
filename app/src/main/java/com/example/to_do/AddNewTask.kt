ddd087743ac86492b25cc9b3f9479dc3b596f0c2
hansikaDS <hansikasenanayaka07@gmail.com>
2024-05-10 18:42:27 +0530

    Update AddNewTask.kt
    override oncreateview() and onviewcreated() functions.

--- app/src/main/java/com/example/to_do/AddNewTask.kt
+++ app/src/main/java/com/example/to_do/AddNewTask.kt
@@ -1,4 +1,43 @@
 package com.example.to_do
 
-class AddNewTask {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.example.to_do.Utils.DataBaseHelper
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+class AddNewTask : BottomSheetDialogFragment() {
+
+    companion object {
+        const val TAG = "AddNewTask"
+    }
+
+    // widgets
+
+    private lateinit var editText: EditText
+    private lateinit var btn_save: Button
+
+    private lateinit var myDb: DataBaseHelper
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? { //parameters can be nullable
+        val v = inflater.inflate(R.layout.add_newtask, container, false)
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editText = view.findViewById(R.id.edittext)
+        btn_save = view.findViewById(R.id.btn_save)
+
+        myDb = DataBaseHelper(requireActivity())
+
+        val isUpdate = false
+    }
+
+
+
 }
